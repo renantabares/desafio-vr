fa10b5b2ee2e678ee24042a36f7edea344b5b72a
renantmachado <renantabares@homail.com>
2025-09-24 16:27:15 -0300

    add new project 'vr-authorization-server

--- vr-authorization-server/src/test/java/br/com/vr/VrAuthorizationServerApplicationTests.java
+++ vr-authorization-server/src/test/java/br/com/vr/VrAuthorizationServerApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServerApplicationTests {
+class VrAuthorizationServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
